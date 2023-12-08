042a68908146d1ea3bf2973f2b45b7f9189a2094
topzyray <topzyray@yahoo.com>
2023-12-08 19:34:13 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+
+inquirer
+  .prompt([
+    {
+        message: "Type in your URL: ",
+        name: "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    let qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+    fs.writeFile("URL.txt", url, (err) => {
+        if (err) throw err;
+        console.log(`The was saved successfully`)
+    })
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
